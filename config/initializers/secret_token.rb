3d2d1b37fec68f84dcb450c7bb66e51f395e46b9
麥町 <udonmai@gmail.com>
2014-07-14 15:01:01 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rbchat::Application.config.secret_token = '4c9e5af6078ad7a8e333d839f49e8f9fae5ab9df6ecd8a1085502a8adbf15df0046a8ffeb1367ebed5ad5c8826a0fc17fa4f0f502eddf98f75dd238f551cb9f4'
