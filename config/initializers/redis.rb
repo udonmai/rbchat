4ab71dcdf79f54f29e8fc1fe5735160d1f24eaec
udonmai <udonmai@gmail.com>
2014-07-19 17:27:30 +0900

    heroku redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,5 +2,5 @@
 #Redis = Redis.new(:host => 'localhost', :port => 6379)
 
 # Heroku
-Redis = Redis.new(:host => 'localhost', :port => 9070)
+Redis = Redis.new(:host => 'redis://redistogo:9dc8f46f1ed6e688b1fbe082d9495ae8@spadefish.redistogo.com', :port => 9070)
 
