e7b6ce2e1f0649c03cb6a19255f2535634e790e0
udonmai <udonmai@gmail.com>
2014-07-19 17:40:28 +0900

    heroku redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,9 @@
 # Local
-#Redis = Redis.new(:host => 'localhost', :port => 6379)
+# Redis = Redis.new(:host => 'localhost', :port => 6379)
+
+uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
+Redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 # Heroku
-Redis = Redis.new(:host => 'redis://redistogo:9dc8f46f1ed6e688b1fbe082d9495ae8@spadefish.redistogo.com', :port => 9070)
+# Redis = Redis.new(:host => 'redis://redistogo:9dc8f46f1ed6e688b1fbe082d9495ae8@spadefish.redistogo.com', :port => 9070)
 
