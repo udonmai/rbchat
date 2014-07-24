71624ca45dee00e2593ca2375ac62e2605dd0e73
udonmai <udonmai@gmail.com>
2014-07-24 21:29:35 +0900

    suplement

---
+++ public/static/js/loginit.js
@@ -0,0 +1,28 @@
+seajs.config({
+	alias: {
+		'es5-safe': 'es5-safe-0.9.0/es5-safe',
+		'json': 'json-1.0.0/json',
+		'cookie': 'cookie-1.0.2/cookie',
+		'querystring': 'querystring-1.0.1/querystring',
+		'jquery': 'jquery-1.7.2/jquery'
+	},
+	preload: [
+		Function.prototype.bind ? '' : 'es5-safe',
+		this.JSON ? '' : 'json'
+	],
+	debug: true,
+	base: '/static/js/lib/',
+	charset: 'utf-8'
+});
+
+define(function(require, exports, module) {
+	var $ = require('jquery');
+	var Cookie = require('cookie');
+
+	$(document).ready(function() {
+		$('#in a').click(function(event) {
+			$('#action form').slideDown();
+		});
+
+	});	
+});
